84d8805d573e7028fc3c950e0c330d25e7ac1843
hyadess <sayemshahadsoummo@gmail.com>
2023-08-21 04:14:57 +0600

    OFFLINE-3 version7

--- OFFLINE-3/offline_3_maxCut.cpp
+++ OFFLINE-3/offline_3_maxCut.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define ll long long
 #define NO_OF_NODES 5000
-#define ITERATION_COUNT 500
+#define ITERATION_COUNT 1200
 
 struct Edge
 {
@@ -20,17 +20,22 @@ struct Result
     /// greedy max-cut result
     ll greedyPartitionResult;
     // <local search type,<iterationCount,max-cut result>>
-    vector<pair<ll, pair<ll, ll>>> localSearchResults;
+    // O for randomized partitions, 1 for greedy partitions, 2 for semi-greedy partitions.....
+    vector<pair<ll, pair<ll, ll>>> localSearchResults[3];
 };
 
 int nodePartitionTrack[NO_OF_NODES]; // 0 if not partitioned, 1 if partition 1, 2 if partition 2.......
 int bestNodePartition[NO_OF_NODES];
 ll bestAns = -1e18;
 int nodes;
+int edgeNo;
+ll maxWeight = -1e18,ansItr=-1;
+ll minWeight = 1e18;
 vector<pair<ll, ll>> edges[NO_OF_NODES];
 
 Result result;
 int currentIteration;
+int currentPartitionType;
 
 // random generations******************************************************************
 double randomAlphaGeneration()
@@ -374,13 +379,22 @@ void random_partition_simulation()
 
 void greedyStage()
 {
-    int ran = randomInRange(0, 10);
-    if (ran <= 6)
+    int ran = randomInRange(0, 20);
+    if (ran <= 11)
+    {
+        currentPartitionType = 2;
         semi_greedy_partition_simulation();
-    else if (ran <= 8)
+    }
+    else if (ran <= 17)
+    {
+        currentPartitionType = 0;
         random_partition_simulation();
+    }
     else
+    {
+        currentPartitionType = 1;
         greedy_partition_simulation();
+    }
 }
 
 // ###################################LOCAL SEARCH STAGE#####################################################
@@ -482,16 +496,16 @@ void localSearchOnPartition()
         }
     }
     bestAns = bestAnswer;
-    result.localSearchResults.push_back({c, {total_iteration, bestAnswer}});
+    result.localSearchResults[currentPartitionType].push_back({c, {total_iteration, bestAnswer}});
 
     // cout << "max cut: " << bestAns << endl;
 }
 
-ll oneIteration()
+void oneIteration()
 {
     greedyStage();
     localSearchOnPartition();
-    return bestAns;
+    
 }
 
 void printResult()
@@ -500,111 +514,142 @@ void printResult()
          << endl;
     cout << setw(20) << left << "iteration no" << setw(20) << left << "alpha" << setw(20) << left << "max-cut" << endl;
 
-    double averageAlpha=0;
-    double averageSemiGreedyMaxCut=0;
+    double averageAlpha = 0;
+    double averageSemiGreedyMaxCut = 0;
     for (int i = 0; i < result.semiGreedyPartitionResults.size(); i++)
     {
-        averageAlpha+=result.semiGreedyPartitionResults[i].second.first;
-        averageSemiGreedyMaxCut+=result.semiGreedyPartitionResults[i].second.second;
+        averageAlpha += result.semiGreedyPartitionResults[i].second.first;
+        averageSemiGreedyMaxCut += result.semiGreedyPartitionResults[i].second.second;
         cout << setw(20) << left << result.semiGreedyPartitionResults[i].first
              << setw(20) << left << result.semiGreedyPartitionResults[i].second.first
              << setw(20) << left << result.semiGreedyPartitionResults[i].second.second << endl;
     }
-    averageAlpha=1.0*averageAlpha/result.semiGreedyPartitionResults.size();
-    averageSemiGreedyMaxCut=1.0*averageSemiGreedyMaxCut/result.semiGreedyPartitionResults.size();
+    averageAlpha = 1.0 * averageAlpha / result.semiGreedyPartitionResults.size();
+    averageSemiGreedyMaxCut = 1.0 * averageSemiGreedyMaxCut / result.semiGreedyPartitionResults.size();
     cout << endl
          << endl;
 
     cout << "RANDOM PARTITION RESULTS------------------------------>"
          << endl;
     cout << setw(20) << left << "iteration no" << setw(20) << left << "max-cut" << endl;
-    double averageRandomMaxCut=0;
+    double averageRandomMaxCut = 0;
     for (int i = 0; i < result.randomizedPartitionResults.size(); i++)
     {
-        averageRandomMaxCut+=result.randomizedPartitionResults[i].second;
+        averageRandomMaxCut += result.randomizedPartitionResults[i].second;
         cout << setw(20) << left << result.randomizedPartitionResults[i].first
              << setw(20) << left << result.randomizedPartitionResults[i].second << endl;
     }
-    averageRandomMaxCut=1.0*averageRandomMaxCut/result.randomizedPartitionResults.size();
+    averageRandomMaxCut = 1.0 * averageRandomMaxCut / result.randomizedPartitionResults.size();
     cout << endl
          << endl;
 
     cout << "LOCAL SEARCH RESULTS------------------------------>"
          << endl;
     cout << setw(20) << left << "local search type" << setw(20) << left << "iteration count" << setw(20) << left << "max-cut" << endl;
-    int vp=0,firstType=0,secondType=0;
-    double averageFirstTypeItrCount=0,firstTypeMaxCut=0;
-    double averageSecondTypeItrCount=0,secondTypeMaxCut=0;
+    int vp = 0, firstType = 0, secondType = 0,partitionType[3]={0};
+    double FirstTypeItrCount[3] = {0}, firstTypeMaxCut[3] = {0};
+    double SecondTypeItrCount[3] = {0}, secondTypeMaxCut[3] = {0};
 
-    for (int i = 0; i < result.localSearchResults.size(); i++)
+    for (int t = 0; t < 3; t++)
     {
-        vp++;
-        if(result.localSearchResults[i].first==1)
+        for (int i = 0; i < result.localSearchResults[t].size(); i++)
         {
-            firstType++;
-            averageFirstTypeItrCount+=result.localSearchResults[i].second.first;
-            firstTypeMaxCut+=result.localSearchResults[i].second.second;
-
+            vp++;
+            if (result.localSearchResults[t][i].first == 1)
+            {
+                firstType++;
+                FirstTypeItrCount[t] += result.localSearchResults[t][i].second.first;
+                firstTypeMaxCut[t] += result.localSearchResults[t][i].second.second;
+            }
+            else
+            {
+                secondType++;
+                SecondTypeItrCount[t] += result.localSearchResults[t][i].second.first;
+                secondTypeMaxCut[t] += result.localSearchResults[t][i].second.second;
+            }
+            cout << setw(20) << left << result.localSearchResults[t][i].first
+                 << setw(20) << left << result.localSearchResults[t][i].second.first
+                 << setw(20) << left << result.localSearchResults[t][i].second.second << endl;
         }
-        else
-        {
-            secondType++;
-            averageSecondTypeItrCount+=result.localSearchResults[i].second.first;
-            secondTypeMaxCut+=result.localSearchResults[i].second.second;
-        }
-        cout << setw(20) << left << result.localSearchResults[i].first
-             << setw(20) << left << result.localSearchResults[i].second.first
-             << setw(20) << left << result.localSearchResults[i].second.second << endl;
     }
-    averageFirstTypeItrCount=1.0*averageFirstTypeItrCount/firstType;
-    firstTypeMaxCut=1.0*firstTypeMaxCut/firstType;
 
-    averageSecondTypeItrCount=1.0*averageSecondTypeItrCount/secondType;
-    secondTypeMaxCut=1.0*secondTypeMaxCut/secondType;
+    double avgFirstTypeItr=0,avgSecondTypeItr=0,avgFirstTypeMaxCut=0,avgSecondTypeMaxCut=0;
+    for(int i=0;i<3;i++)
+    {
+        avgFirstTypeItr+=FirstTypeItrCount[i];
+        avgSecondTypeItr+=SecondTypeItrCount[i];
+        avgFirstTypeMaxCut+=firstTypeMaxCut[i];
+        avgSecondTypeMaxCut+=secondTypeMaxCut[i];
+    }
+    
+
+
+    
 
     cout << endl
          << endl;
 
-    FILE* file=freopen("outputs/partitionOutput.csv", "a", stdout);
-    //cout<<"Average alpha for semi greedy: ";
-    cout<<averageAlpha<<",";
-    //cout<<"Average max cut for semi greedy: ";
-    cout<<averageSemiGreedyMaxCut<<","; 
-    //cout<<"Average max cut  for random: ";
-    cout<<averageRandomMaxCut<<",";
-    cout<<result.greedyPartitionResult;
-    cout<<endl;
+    FILE *file = freopen("outputs/partitionOutput.csv", "a", stdout);
 
+    // PARTITION RESULTS.....................................
+
+    cout << nodes << "," << edgeNo << "," << minWeight << "," << maxWeight << ",";
+    // cout<<"Average alpha for semi greedy: ";
+    cout << averageAlpha << ",";
+    // cout<<"Average max cut for semi greedy: ";
+    cout << averageSemiGreedyMaxCut << ",";
+    // cout<<"Average max cut  for random: ";
+    cout << averageRandomMaxCut << ",";
+    // average maxcut for greedy
+    cout << result.greedyPartitionResult;
+    cout << endl;
     fclose(file);
 
-    
-    file=freopen("outputs/remainingOutput.csv", "a", stdout);
 
+    file = freopen("outputs/localSearchVsPartitionType.csv", "a", stdout);
 
-    //cout<<"Average iteration count for best chose local search: ";
-    cout<<averageFirstTypeItrCount<<",";
-    //cout<<"Average max cut for best chose local search: ";
-    cout<<firstTypeMaxCut<<",";
-    //cout<<"Average iteration count for first chose local search: ";
-    cout<<averageSecondTypeItrCount<<",";
-    //cout<<"Average max cut for first chose local search: ";
-    cout<<secondTypeMaxCut<<",";
-    cout<<bestAns;
+    cout << nodes << "," << edgeNo << "," << minWeight << "," << maxWeight << ",";
+    //for randomized
+    cout<<1.0*(FirstTypeItrCount[0]+SecondTypeItrCount[0])/result.localSearchResults[0].size()<<","<<1.0*(firstTypeMaxCut[0]+secondTypeMaxCut[0])/result.localSearchResults[0].size()<<",";
+    //for greedy
+    cout<<1.0*(FirstTypeItrCount[1]+SecondTypeItrCount[1])/result.localSearchResults[1].size()<<","<<1.0*(firstTypeMaxCut[1]+secondTypeMaxCut[1])/result.localSearchResults[1].size()<<",";
+    //for semi_greedy
+    cout<<1.0*(FirstTypeItrCount[2]+SecondTypeItrCount[2])/result.localSearchResults[2].size()<<","<<1.0*(firstTypeMaxCut[2]+secondTypeMaxCut[2])/result.localSearchResults[2].size();
     cout<<endl;
+    fclose(file);
+
+
+
+    file = freopen("outputs/localSearchComp.csv", "a", stdout);
+
+    ///LOCAL SEARCH COMPARISON...............
+    cout << nodes << "," << edgeNo << "," << minWeight << "," << maxWeight << ",";
+    // cout<<"Average iteration count for best chose local search: ";
+    cout << 1.0*avgFirstTypeItr/firstType<< ",";
+    // cout<<"Average max cut for best chose local search: ";
+    cout << 1.0*avgFirstTypeMaxCut/firstType << ",";
+    // cout<<"Average iteration count for first chose local search: ";
+    cout << 1.0*avgSecondTypeItr/secondType<< ",";
+    // cout<<"Average max cut for first chose local search: ";
+    cout << 1.0*avgSecondTypeMaxCut/secondType<< ",";
+    // GRASP RESULTS....................
+    cout <<ITERATION_COUNT<<","<< bestAns<<","<<ansItr<<","<<currentPartitionType;
+    cout << endl;
 
     fclose(file);
 }
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
     freopen(argv[1], "r", stdin);
-    
-    int m;
-    cin >> nodes >> m;
-    for (int i = 0; i < m; i++)
+
+    cin >> nodes >> edgeNo;
+    for (int i = 0; i < edgeNo; i++)
     {
         ll l, r, w;
         cin >> l >> r >> w;
+        minWeight = min(minWeight, w);
+        maxWeight = max(maxWeight, w);
         edges[l].push_back({r, w});
         edges[r].push_back({l, w});
     }
@@ -614,10 +659,11 @@ int main(int argc,char* argv[])
         currentIteration++;
         // cout << "****************************ITERATION " << i << "*********************************" << endl;
         bestAns = -1e18;
-        ll p = oneIteration();
-        if (p > ans)
+        oneIteration();
+        if (bestAns > ans)
         {
-            ans = p;
+            ans = bestAns;
+            ansItr=i;
         }
         // cout << endl
         //      << endl
@@ -625,7 +671,7 @@ int main(int argc,char* argv[])
         //      << endl
         //      << endl;
     }
-    bestAns=ans;
+    bestAns = ans;
     printResult();
     cout << "BEST ANS AFTER ALL: " << ans << endl;
 }
--- OFFLINE-3/run.sh
+++ OFFLINE-3/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-g++ -c  offline_3_maxCut.cpp -o e.o
-g++ e.o -o e.exe
+g++ -c -O9 offline_3_maxCut.cpp -o e.o
+g++ -O9 e.o -o e.exe
 rm -r e.o
 
-for ((i = 41; i <= 54; i++)); do
+for ((i = 1; i <= 54; i++)); do
     input="inputs/g$i.rud"
     echo "$input"
     ./e.exe "$input"
 done 
 
-shutdown -h now
+#shutdown -h now
